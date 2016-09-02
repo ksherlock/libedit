e29a5c9ea1bcbce7ba093281d24cde46c3358082
Kelvin Sherlock <ksherlock@gmail.com>
2016-09-02 13:34:27 -0400

    remove extra autoconfig crap that isn't actually used.

--- config.h.cmake
+++ config.h.cmake
@@ -1,26 +1,22 @@
 /* config.h.cmake  */
 
-/* Define to 1 if the `closedir' function returns void instead of `int'. */
-#cmakedefine CLOSEDIR_VOID
-
 /* Define to 1 if you have the <curses.h> header file. */
 #cmakedefine HAVE_CURSES_H
 
-/* Define to 1 if you have the <dirent.h> header file, and it defines `DIR'.
-   */
-#cmakedefine HAVE_DIRENT_H
+/* Define to 1 if you have the <ncurses.h> header file. */
+#cmakedefine HAVE_NCURSES_H
 
-/* Define to 1 if you have the <dlfcn.h> header file. */
-#cmakedefine HAVE_DLFCN_H
+/* Define to 1 if you have the <sys/cdefs.h> header file. */
+#cmakedefine HAVE_SYS_CDEFS_H
 
-/* Define to 1 if you have the `endpwent' function. */
-#cmakedefine HAVE_ENDPWENT
+/* Define to 1 if you have the <termcap.h> header file. */
+#cmakedefine HAVE_TERMCAP_H
 
-/* Define to 1 if you have the <fcntl.h> header file. */
-#cmakedefine HAVE_FCNTL_H
+/* Define to 1 if you have the <term.h> header file. */
+#cmakedefine HAVE_TERM_H
 
-/* Define to 1 if you have the `fork' function. */
-#cmakedefine HAVE_FORK
+/* Define to 1 if you have the <stdint.h> header file. */
+#cmakedefine HAVE_STDINT_H
 
 /* Define to 1 if you have the `getline' function. */
 #cmakedefine HAVE_GETLINE
@@ -33,84 +29,14 @@
    compatible. */
 #cmakedefine HAVE_GETPW_R_POSIX
 
-/* Define to 1 if you have the <inttypes.h> header file. */
-#cmakedefine HAVE_INTTYPES_H
-
-/* Define to 1 if you have the `isascii' function. */
-#cmakedefine HAVE_ISASCII
-
 /* Define to 1 if you have the `issetugid' function. */
 #cmakedefine HAVE_ISSETUGID
-
-/* Define to 1 if you have the `curses' library (-lcurses). */
-#cmakedefine HAVE_LIBCURSES
-
-/* Define to 1 if you have the `ncurses' library (-lncurses). */
-#cmakedefine HAVE_LIBNCURSES
-
-/* Define to 1 if you have the `termcap' library (-ltermcap). */
-#cmakedefine HAVE_LIBTERMCAP
-
-/* Define to 1 if you have the <limits.h> header file. */
-#cmakedefine HAVE_LIMITS_H
-
-/* Define to 1 if you have the <malloc.h> header file. */
-#cmakedefine HAVE_MALLOC_H
-
-/* Define to 1 if you have the `memchr' function. */
-#cmakedefine HAVE_MEMCHR
-
-/* Define to 1 if you have the <memory.h> header file. */
-#cmakedefine HAVE_MEMORY_H
-
-/* Define to 1 if you have the `memset' function. */
-#cmakedefine HAVE_MEMSET
-
-/* Define to 1 if you have the <ncurses.h> header file. */
-#cmakedefine HAVE_NCURSES_H
-
-/* Define to 1 if you have the <ndir.h> header file, and it defines `DIR'. */
-#cmakedefine HAVE_NDIR_H
-
-/* Define to 1 if you have the `regcomp' function. */
-#cmakedefine HAVE_REGCOMP
-
-/* Define to 1 if you have the `re_comp' function. */
-#cmakedefine HAVE_RE_COMP
 
 /* Define to 1 if you have the `secure_getenv' function. */
 #cmakedefine HAVE_SECURE_GETENV
 
-/* Define to 1 if `stat' has the bug that it succeeds when given the
-   zero-length file name argument. */
-#cmakedefine HAVE_STAT_EMPTY_STRING_BUG
-
-/* Define to 1 if you have the <stdint.h> header file. */
-#cmakedefine HAVE_STDINT_H
-
-/* Define to 1 if you have the <stdlib.h> header file. */
-#cmakedefine HAVE_STDLIB_H
-
-/* Define to 1 if you have the `strcasecmp' function. */
-#cmakedefine HAVE_STRCASECMP
-
-/* Define to 1 if you have the `strchr' function. */
-#cmakedefine HAVE_STRCHR
-
-/* Define to 1 if you have the `strcspn' function. */
-#cmakedefine HAVE_STRCSPN
-
-/* Define to 1 if you have the `strdup' function. */
-#cmakedefine HAVE_STRDUP
-
-/* Define to 1 if you have the `strerror' function. */
-#cmakedefine HAVE_STRERROR
-
-/* Define to 1 if you have the <strings.h> header file. */
-#cmakedefine HAVE_STRINGS_H
-
-/* Define to 1 if you have the <string.h> header file. */
-#cmakedefine HAVE_STRING_H
+/* Define to 1 if you have the `__secure_getenv' function. */
+#cmakedefine HAVE___SECURE_GETENV
 
 /* Define to 1 if you have the `strlcat' function. */
 #cmakedefine HAVE_STRLCAT
@@ -118,89 +44,17 @@
 /* Define to 1 if you have the `strlcpy' function. */
 #cmakedefine HAVE_STRLCPY
 
-/* Define to 1 if you have the `strrchr' function. */
-#cmakedefine HAVE_STRRCHR
-
-/* Define to 1 if you have the `strstr' function. */
-#cmakedefine HAVE_STRSTR
-
-/* Define to 1 if you have the `strtol' function. */
-#cmakedefine HAVE_STRTOL
-
-/* Define to 1 if struct dirent has member d_namlen */
-#cmakedefine HAVE_STRUCT_DIRENT_D_NAMLEN
-
-/* Define to 1 if you have the `strunvis' function. */
-#cmakedefine HAVE_STRUNVIS
-
-/* Define to 1 if you have the `strvis' function. */
-#cmakedefine HAVE_STRVIS
-
-/* Define to 1 if you have the <sys/cdefs.h> header file. */
-#cmakedefine HAVE_SYS_CDEFS_H
-
-/* Define to 1 if you have the <sys/dir.h> header file, and it defines `DIR'.
-   */
-#cmakedefine HAVE_SYS_DIR_H
-
-/* Define to 1 if you have the <sys/ioctl.h> header file. */
-#cmakedefine HAVE_SYS_IOCTL_H
-
-/* Define to 1 if you have the <sys/ndir.h> header file, and it defines `DIR'.
-   */
-#cmakedefine HAVE_SYS_NDIR_H
-
-/* Define to 1 if you have the <sys/param.h> header file. */
-#cmakedefine HAVE_SYS_PARAM_H
-
-/* Define to 1 if you have the <sys/stat.h> header file. */
-#cmakedefine HAVE_SYS_STAT_H
-
-/* Define to 1 if you have the <sys/types.h> header file. */
-#cmakedefine HAVE_SYS_TYPES_H
-
-/* Define to 1 if you have <sys/wait.h> that is POSIX.1 compatible. */
-#cmakedefine HAVE_SYS_WAIT_H
-
-/* Define to 1 if you have the <termcap.h> header file. */
-#cmakedefine HAVE_TERMCAP_H
-
-/* Define to 1 if you have the <term.h> header file. */
-#cmakedefine HAVE_TERM_H
-
-/* Define to 1 if you have the <unistd.h> header file. */
-#cmakedefine HAVE_UNISTD_H
-
 /* Define to 1 if the system has the type `u_int32_t'. */
 #cmakedefine HAVE_U_INT32_T
-
-/* Define to 1 if you have the `vfork' function. */
-#cmakedefine HAVE_VFORK
-
-/* Define to 1 if you have the <vfork.h> header file. */
-#cmakedefine HAVE_VFORK_H
 
 /* Define to 1 if you have the `vis' function. */
 #cmakedefine HAVE_VIS
 
+/* Define to 1 if you have the `unvis' function. */
+#cmakedefine HAVE_UNVIS
+
 /* Define to 1 if you have the `wcsdup' function. */
 #cmakedefine HAVE_WCSDUP
-
-/* Define to 1 if `fork' works. */
-#cmakedefine HAVE_WORKING_FORK
-
-/* Define to 1 if `vfork' works. */
-#cmakedefine HAVE_WORKING_VFORK
-
-/* Define to 1 if you have the `__secure_getenv' function. */
-#cmakedefine HAVE___SECURE_GETENV
-
-/* Define to 1 if `lstat' dereferences a symlink specified with a trailing
-   slash. */
-#cmakedefine LSTAT_FOLLOWS_SLASHED_SYMLINK
-
-/* Define to the sub-directory where libtool stores uninstalled libraries. */
-#cmakedefine LT_OBJDIR "@LT_OBJDIR@"
 
 /* Name of package */
 #cmakedefine PACKAGE "@PACKAGE@"
@@ -223,11 +77,7 @@
 /* Define to the version of this package. */
 #cmakedefine PACKAGE_VERSION "@PACKAGE_VERSION@"
 
-/* Define as the return type of signal handlers (`int' or `void'). */
-#cmakedefine RETSIGTYPE @RETSIGTYPE@
 
-/* Define to 1 if you have the ANSI C header files. */
-#cmakedefine STDC_HEADERS
 
 /* Enable extensions on AIX 3, Interix.  */
 #ifndef _ALL_SOURCE
@@ -254,27 +104,6 @@
 /* Version number of package */
 #cmakedefine VERSION "@VERSION@"
 
-/* Define to 1 if on MINIX. */
-#cmakedefine _MINIX
-
-/* Define to 2 if the system does not provide POSIX.1 features except with
-   this defined. */
-#cmakedefine _POSIX_1_SOURCE
-
-/* Define to 1 if you need to in order for `stat' and other things to work. */
-#cmakedefine _POSIX_SOURCE
-
-/* Define to empty if `const' does not conform to ANSI C. */
-#cmakedefine const
-
-/* Define to `int' if <sys/types.h> does not define. */
-#cmakedefine pid_t
-
-/* Define to `unsigned int' if <sys/types.h> does not define. */
-#cmakedefine size_t
-
-/* Define as `fork' if `vfork' does not work. */
-#cmakedefine vfork
 
 
 #include "sys.h"
